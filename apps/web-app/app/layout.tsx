137a6f757bb63385a595f3ead777f0692a72b5a6
Gaurav-CloudActiveLabs <gaurav.chand@cloudactivelabs.com>
2024-09-30 18:44:40 +0530

    meta tags

--- apps/web-app/app/layout.tsx
+++ apps/web-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { Jost } from "next/font/google";
+import OGImage from "@/assets/ogimage.png";
+import favicon from "@/assets/favicon.png";
 
 const jost = Jost({
   subsets: ["latin"],
@@ -9,8 +11,42 @@ const jost = Jost({
 });
 
 export const metadata: Metadata = {
-  title: "SNAS RETREAT - Luxury Wellness Redefined",
-  description: "SNAS RETREAT - Luxury Wellness Redefined",
+  title:
+    "SNAS RETREAT - Luxury Boutique Hotel in Manali | Breathtaking Mountain Views & Premium Stay",
+  description:
+    "Experience luxury at SNAS RETREAT, a boutique hotel in Manali offering stunning mountain views, spacious rooms, fine dining, and world-class amenities. Book your perfect getaway today!",
+  openGraph: {
+    title:
+      "SNAS RETREAT - Luxury Boutique Hotel in Manali | Breathtaking Mountain Views & Premium Stay",
+    description:
+      "Experience luxury at SNAS RETREAT, a boutique hotel in Manali offering stunning mountain views, spacious rooms, fine dining, and world-class amenities. Book your perfect getaway today!",
+    url: "https://snasretreat.com/",
+    siteName: "SNAS RETREAT, Manali",
+    images: [
+      {
+        url: OGImage.src, // Add the URL of your image here
+        width: 800,
+        height: 600,
+        alt: "SNAS RETREAT Hotel View",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary",
+    site: "@SNAS_RETREAT",
+    title:
+      "SNAS RETREAT - Luxury Boutique Hotel in Manali | Breathtaking Mountain Views & Premium Stay",
+    description:
+      "Experience luxury at SNAS RETREAT, a boutique hotel in Manali offering stunning mountain views, spacious rooms, fine dining, and world-class amenities. Book your perfect getaway today!",
+    images: [
+      {
+        url: OGImage.src, // Add the URL of your image here
+        alt: "SNAS RETREAT Hotel View",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -20,6 +56,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href={favicon.src} sizes="16x16" type="image/png" />
+      </head>
       <body className={`${jost.variable} antialiased`}>{children}</body>
     </html>
   );
--- apps/web-app/components/pageComponents/footer.tsx
+++ apps/web-app/components/pageComponents/footer.tsx
@@ -26,7 +26,7 @@ export default function Footer() {
             <div>
               <h2 className="text-2xl font-bold mb-2 sm:align-middle">Our Brand</h2>
               <div className="-ml-12 mt-6">
-                <LogoSquareWithoutBg width={250} height={250}/>
+                <LogoSquareWithoutBg width={240} height={240}/>
               </div>
             </div>
 
