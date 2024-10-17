ca75eeca0056702223a38b4f0833abdf2e2c0a8c
itsmegc <gc.27@outlook.com>
2024-10-17 14:06:06 +0530

    api folder

--- services/api/utils/helper.ts
+++
@@ -1,6 +0,0 @@
-export const generateReceiptId = () => {
-  const timestamp = Date.now(); // Get current timestamp
-  const uniqueId = Math.random().toString(36).substr(2, 9); // Generate a random alphanumeric string
-
-  return `${timestamp}-${uniqueId}`;
-};
